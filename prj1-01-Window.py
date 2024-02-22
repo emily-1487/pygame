ff32f55712ae1c7e27f7a726465cdbbaddd86a08
emily-1487 <emilyliu11022011@gmail.com>
2024-02-22 21:23:50 +0800

    class1

--- prj1-01-Window.py
+++ prj1-01-Window.py
@@ -36,20 +36,35 @@ while True:
     # 畫橢圓, (畫布, 顏色, [x, y, 寬, 高], 線寬)
     pygame.draw.ellipse(bg,(255,0,0),[130,160,60,35],5)
     # 畫線, (畫布, 顏色, 起點, 終點, 線寬)
-    pygame.draw.line(bg,(125,50,100),(100,100),(320,220),3)
+    #pygame.draw.line(bg, (255, 0, 255), (280, 220), (220, 220), 3)
     # 畫多邊形, (畫布, 顏色, [[x1, y1], [x2, y2], [x3, y3]], 線寬)
     pygame.draw.polygon(bg,(125,50,100),[[100,100],[0,200],[200,200]],5)
     # 畫圓弧, (畫布, 顏色, [x, y, 寬, 高], 起始角度, 結束角度, 線寬)
     pygame.draw.arc(bg,(125,50,100),[100,100,100,50],math.radians(180),math.radians(0),2)
+
+
 
     pygame.draw.circle(bg,(255,255,0),(400,300),30,0)
     pygame.draw.circle(bg,(255,255,0),(600,300),30,0)
     pygame.draw.rect(bg,(125,50,100),[470,350,60,40],5)
     pygame.draw.ellipse(bg,(255,0,0),[650,400,60,35],5)
     pygame.draw.ellipse(bg,(255,0,0),[300,400,60,35],5)
-    pygame.draw.line(bg,(125,50,100),(469,464),(320,220),3)
+    pygame.draw.line(bg,(125,50,100),(580,475),(417,475),3)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        pygame.draw.line(bg,(125,50,100),(400,300),(450,300),3)
+        pygame.draw.line(bg,(125,50,100),(600,300),(650,300),3)
+        pygame.draw.rect(bg,(125,50,100),[470,350,60,40],5)
+        pygame.draw.ellipse(bg,(255,0,0),[650,400,60,35],5)
+        pygame.draw.ellipse(bg,(255,0,0),[300,400,60,35],5)
+        pygame.draw.circle(bg,(255,255,0),(498,475),30,0)
 
 
 
+
+    
+    
+    
+    
+    
     pygame.mouse.get_pos()
     x,y=pygame.mouse.get_pos()#存取座標
